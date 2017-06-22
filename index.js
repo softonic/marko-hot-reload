2a4808c44220232a92e095701397b5179ec55396
Rubén Norte <ruben.norte@softonic.com>
2017-06-22 17:47:32 +0200

    Made package compatible with older versions of node.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const markoHotReload = require('marko/hot-reload');
 const chokidar = require('chokidar');
 
-exports.enable = function watchMarkoFiles({ templatesPath }) {
+exports.enable = function watchMarkoFiles(options) {
+  const templatesPath = options.templatesPath;
   markoHotReload.enable();
 
-  chokidar.watch(`${templatesPath}/**/*.marko`)
-    .on('change', path => markoHotReload.handleFileModified(path));
+  chokidar.watch(`${templatesPath}/**/*.marko`).on('change', function(path) {
+    markoHotReload.handleFileModified(path);
+  });
 };
